c9b06a0b6f5eae94c0c26934cca9bb6dea8aaa85
pittacus <pittacus@gmail.com>
2015-08-24 08:37:45 +0800

    Features added
    1. shuffle network edges
    2. compute gene similarity
    3. modify network similarity implememtation
    4. add test script

--- R/calc_sim_score.R
+++ R/calc_sim_score.R
@@ -6,16 +6,15 @@
 #' cluster.
 #' @param gene_sim, gene similarity matrix based on Gene Ontology
 
-calc_cluster_score <- function(cluster,node1,node2,gene_sim){
+calc_cluster_score <- function(cluster,node1,node2){
  v1 <- intersect(node1,names(cluster))
  v2 <- intersect(node2,names(cluster))
- cluster_sim <- gene_sim[cluster,cluster]
- index1 <- match(v1,row.names(gene_sim),nomatch = 0)
- index2 <- match(v2,row.names(gene_sim),nomatch = 0) ## same as setdiff(cluster,index1)
+ index1 <- match(v1,row.names(geneSimData),nomatch = 0)
+ index2 <- match(v2,row.names(geneSimData),nomatch = 0) ## same as setdiff(cluster,index1)
  if (length(index1) && length(index2)){
-   LS1 <- sapply(index1,function(x)max(gene_sim[x,index2])) %>% sum
-   LS2 <- sapply(index2,function(x)max(gene_sim[x,index1])) %>% sum
-   LS <- (LS1 + LS2)/length(cluster)
+   LS1 <- sapply(index1,function(x)max(geneSimData[x,index2])) %>% sum
+   LS2 <- sapply(index2,function(x)max(geneSimData[x,index1])) %>% sum
+   LS <- (LS1 + LS2)/(length(index1)+length(index2))
  }
  else
    LS <- 0
@@ -29,35 +28,52 @@ calc_cluster_score <- function(cluster,node1,node2,gene_sim){
 #' @param ..., all other arguments are passed to apcluster
 
 calc_twonet_score <- function(net1,net2, ...){
+  if(!(exists('geneSimData') && typeof(geneSimData) == "double"))
+    data(geneSimData)
+
+  subgraphwithbp <- function(net){
+    if(!(exists('geneSimData') && typeof(geneSimData) == "double"))
+      data(geneSimData)
+    node <- V(net)$name %>% str_replace('hsa:','')
+    node_index <- node %>% match(row.names(geneSimData),nomatch = 0)
+    node_bp <- row.names(geneSimData)[node_index] %>% paste('hsa:',.,sep='')
+    if(length(node_index) != length(node_bp)) {
+      warning("nodes in network without BP annotation will be ignored!")
+      return(induced.subgraph(net,node_bp))
+    }
+    else
+      return(net)
+  }
   ## merge two networks
+  net1 %<>% subgraphwithbp
+  net2 %<>% subgraphwithbp
+  
   node1 <- V(net1)$name %>% str_replace('hsa:','')
   node2 <- V(net2)$name %>% str_replace('hsa:','')
-  data(geneSimData)
-  node_index <- c(node1,node2) %>% unique %>%
-    match(row.names(geneSimData),nomatch = 0)
-  new_nodes <- row.names(geneSimData)[node_index]
-  edges1 <- get.data.frame(net1)
-  edges1$from <- str_replace(edges1$from,'hsa:','')
-  edges1$to <- str_replace(edges1$to,'hsa:','')
-  edges2 <- get.data.frame(net2)
-  edges2$from <- str_replace(edges2$from,'hsa:','')
-  edges2$to <- str_replace(edges2$to,'hsa:','')
-  new_edges <- rbind(edges1,edges2) %>%
-    filter(is.element(from,new_nodes) & is.element(to,new_nodes))
-  new_graph <- graph.data.frame(new_edges,vertices = new_nodes)
-
+  node_index <- c(node1,node2) %>% match(row.names(geneSimData),nomatch = 0)
   gene_sim <- geneSimData[node_index,node_index]
-  gene_cluster <- apcluster(s = gene_sim, p = mean(preferenceRange(gene_sim), ...))
+  
+  lo=1;hi=length(node1);
+  gene_sim[lo:hi, lo:hi] <- gene_sim[lo:hi, lo:hi] * as.matrix(as_adj(net1))
+  
+  lo=length(node1)+1;hi=length(node1)+length(node2);
+  gene_sim[lo:hi, lo:hi] <- gene_sim[lo:hi, lo:hi] * as.matrix(as_adj(net2))
+  
+  gene_cluster <- apcluster(s = gene_sim)
   ## cluster <- names(gene_cluster[[1]])
   ## calculate the similarity score in a specific cluster
   score <- sapply(1:length(gene_cluster),function(x)calc_cluster_score(gene_cluster[[x]],
-    node1,node2,gene_sim)) %>% mean
+    node1,node2)) %>% mean
   return(score)
 }
 
 #' @title randomize the network
 
 randomize_net <- function(net){
+  #edge shuffle
+  net <- rewire(net, keeping_degseq(niter = gsize(net) * 4)) 
+  
+  #node shuffle
   genes <- row.names(geneSimData)
   index <-  genes %>% length %>% sample(length(V(net)))
   new_node <- genes[index] %>% paste0('hsa:',.)
@@ -101,6 +117,7 @@ standardize_score <- function(net,refnet,nperm,...){
 #' @export
 
 
+
 calc_sim_score <- function(net,refnet,nperm = 100, plot = TRUE, ...){
   if (class(net) != 'igraph')
     stop("Please ensure your network is in graphNEL class")
@@ -115,13 +132,16 @@ calc_sim_score <- function(net,refnet,nperm = 100, plot = TRUE, ...){
     random_score_E <- mean(random_score)
     random_score_SD <- sd(random_score)
   }
+
   sim <- (sim_score - random_score_E)/random_score_SD
+
   if (is.null(names(refnet)))
     sim_df <- data.frame(sim = sim, pathway = 1:length(sim))
   else
     sim_df <- data.frame(sim = sim, pathway = names(refnet))
-  if (plot)
+  if (plot && !is.na(sim)){ # skip plot if sim is NA
     print(ggplot(sim_df,aes(x=pathway,y=sim)) + geom_point(size=3) +
       geom_segment(aes(xend=pathway,yend=0),size=1))
+  }
   return(sim)
 }
--- R/loadKEGGRefNet.R
+++ R/loadKEGGRefNet.R
@@ -7,7 +7,7 @@
 #'
 #' @export
 
-loadKEGGRefNet <- function(){
+getPathwayList <- function(){
   pathway_ID <- system.file('extdata/signalPathway.txt',package='NFP') %>%
     read.delim(stringsAsFactors = FALSE,header = FALSE,colClasses = 'character') %>%
     extract2(1)
@@ -17,8 +17,24 @@ loadKEGGRefNet <- function(){
     extract2(1)
   human_signalpathway <- intersect(pathway_ID,human_pathway)
   human_signalpathway <- human_signalpathway[-125] ## ? bug
+  return(human_signalpathway)
+}
+loadKEGGRefNet <- function(){
+  human_signalpathway <- getPathwayList()
   KEGG_graph <- human_signalpathway %>% laply(getKGMLurl,organism = 'hsa') %>%
     llply(parseKGML2Graph,.progress = 'text') %>% llply(igraph.from.graphNEL)
   names(KEGG_graph) <- human_signalpathway
   return(KEGG_graph)
+}
+exportKEGGRefNet <- function() {
+  saveKEGGNet4Matlab <- function(pathway_id){
+    net <- pathway_id %>% getKGMLurl(organism = 'hsa') %>% parseKGML2Graph %>% igraph.from.graphNEL
+    n=net %>% as_adj %>% as.matrix %>% row.names %>% str_replace('hsa:','') %>% as.double
+    e=net %>% as_adj %>% as.matrix 
+    #writeMat(paste("hsa",pathway_id,".mat",sep=""), node=n, adj=e)
+    #write.csv(node,file=paste("hsa",pathway_id,".node",sep=""))
+    write.csv(e,paste("hsa",pathway_id,".adj",sep="")) 
+  }
+
+  getPathwayList() %>% llply(saveKEGGNet4Matlab,.progress = 'text')
 }
